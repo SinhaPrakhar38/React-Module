e7a61a4841e1384324ed8db3537c9b615c855af1
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-05-19 13:19:29 +0530

    Create List.jsx

---
+++ import React from "react";  function List() {   return (     <ul>       <li>Jamon</li>       <li>Noodles</li>       <li>Chicken</List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Jamon</li>
+      <li>Noodles</li>
+      <li>Chicken</li>
+    </ul>
+  );
+}
+
+export default List;
